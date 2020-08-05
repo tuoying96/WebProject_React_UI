46d0b677bb5af58c7007070fc6a1f781b607fa2b
tuoying96 <tuoying96@gmail.com>
2020-08-05 15:07:57 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
 const proxy = require("http-proxy-middleware");
 
-module.exports = function(app) {
-  app.use(
-    proxy(["/api", , "/otherApi"], { target: "https://webproject-ui.herokuapp.com/" })
-  );
-};
+module.exports = app => {
+    app.use(proxy("/api/*", { target: "https://webproject-ui.herokuapp.com/" }));
+  };
+
+// module.exports = function(app) {
+//   app.use(
+//     proxy(["/api", , "/otherApi"], { target: "https://webproject-ui.herokuapp.com/" })
+//   );
+// };
