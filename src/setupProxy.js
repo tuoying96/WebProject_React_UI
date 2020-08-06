c562f80021198c6906d190b8daf35f0010b12283
tuoying96 <tuoying96@gmail.com>
2020-08-05 21:01:59 -0400

    use procy

---
+++ src/setupProxy.js
@@ -0,0 +1,17 @@
+// const {createProxyMiddleware} = require("http-proxy-middleware");
+
+// module.exports = app => {
+//     app.use('/api', createProxyMiddleware({target: "https://webproject-ui.herokuapp.com/login"
+//  }));
+// };
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'https://webproject-ui.herokuapp.com',
+      changeOrigin: true,
+    })
+  );
+};
